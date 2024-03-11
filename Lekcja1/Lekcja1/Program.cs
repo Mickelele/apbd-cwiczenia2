da23b7a30315eccd9a9ab5d6f43f87cc3edafc91
s28517 <s28517@pjwstk.edu.pl>
2024-03-12 00:58:19 +0100

    Dodano funkcje zwracajaca maksymalny element tablicy

--- Lekcja1/Lekcja1/Program.cs
+++ Lekcja1/Lekcja1/Program.cs
@@ -10,9 +10,20 @@ static double GetAverage(int[] arr)
     return (double)suma/arr.Length;
 }
 
-int[] arr = { 1,2,3,4 };
-double avg = GetAverage(arr);
-Console.WriteLine(avg);
+int[] arr = { 5,7,44,435,59043 };
+//double avg = GetAverage(arr);
+//Console.WriteLine(avg);
 
 
-Console.WriteLine("DOM");
+static int GetMax(int[] arr)
+{
+    int max = arr[0];
+    foreach (var elem in arr)
+    {
+        if (elem > max) max = elem;
+    }
+    return max;
+}
+
+int max = GetMax(arr);
+Console.WriteLine(max);
